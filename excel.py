f50a1dca5aa0c3ba84dab52301b6ecb0dee79b92
janar <janarthanamr@yahoo.com>
2014-02-02 14:37:19 -0800

    introduce a special case where findings are numbered and there are two of them and details are not numbered in both the columns + incluce : as a numeric separator

--- excel.py
+++ excel.py
@@ -15,15 +15,18 @@ from xlrd import XLRDError
 # This will be the only dependency with xlwt
 
 ROW = 0
-FINDING = 1
-FINDING_DETAIL = 2
-COMMENT = 3
+ID = 1
+FINDING = 2
+FINDING_DETAIL = 3
+COMMENT = 4
 
-def write_row = 0
+write_rw = 0
 
-def write_row(ws,row_num, finding, detail, comment):
+def write_row(ws,row_num, id,finding, detail, comment):
     global write_rw
+
     ws.write(write_rw,ROW,row_num)
+    ws.write(write_rw,ID,id)
     ws.write(write_rw,FINDING,finding)
     ws.write(write_rw,FINDING_DETAIL,detail)
     ws.write(write_rw,COMMENT,comment)
@@ -36,7 +39,7 @@ def extract_numbered_entries(blob):
     buf = ''
 
     for line in lines:
-        if re.match('^\d\s*[.)].*',line): #beginning of a new numbered list
+        if re.match('^\d\s*[.):].*',line): #beginning of a new numbered list
             if buf and numbered: #if its not the first numbered list
                 numbered_list.append(buf) #push the last numbered list
                 buf = '' #empty buffer
@@ -66,15 +69,15 @@ for ifile in ifiles:
     #define writer
     #TOD0: encapsulate the creation and initialization in to a method in a class
     # leads to better abstraction of reads and writes. Easier to debug!!
-    write_row = 0  
+    write_rw = 0  
     wb = Workbook()
     ws = wb.add_sheet('0')
-    write_row(ws0,"Row #","Finding","Finding Detail", "Comment")
+    write_row(ws,"Row #","ID", "Finding","Finding Detail", "Comment")
 
     for rx in range(2,sheet.nrows):
         finding = sheet.cell_value(rowx=rx,colx=4) #Column E, row = rx
         #print finding;
-        if finding.lower() == "n/a":
+        if finding.lower() == "n/a" or finding.strip() == '':
             continue
 
         findings = finding.splitlines();
@@ -82,6 +85,7 @@ for ifile in ifiles:
 
         findingdetail1 = sheet.cell_value(rowx=rx,colx=5) #Column F, row=rx
         findingdetail2 = sheet.cell_value(rowx=rx,colx=6) #Column G, row=rx
+        id = sheet.cell_value(rowx=rx,colx=1) #Column A, row=rx
 
         d1 = findingdetail1.strip()  #remove leading whitespace
         d2 = findingdetail2.strip()  #remove leading whitespace
@@ -96,21 +100,27 @@ for ifile in ifiles:
         if( numbered_findings and numbered_finding_details and len(numbered_findings) == len(numbered_finding_details)):
             for i in range(len(numbered_findings)):
                 #print u"Row:%d:cond3 numeric:" % (rx) + numbered_findings[i] + "\t" + numbered_finding_details[i]
-                write_row(ws,rx+1,numbered_findings[i],numbered_finding_details[i],"Cond3: Numeric match")
+                write_row(ws,rx+1,id,numbered_findings[i],numbered_finding_details[i],"Cond3: Numeric match")
         #Cond2 - two findings with d1 and d2
         elif not numbered_findings:
             #equality check for 2 - code named Cond2
             if len(findings) == 2 and d1 != '' and d2 != '':
-                write_row(ws,rx+1,findings[0],d1,"Cond2")
-                write_row(ws,rx+1,findings[1],d2,"Cond2")
+                write_row(ws,rx+1,id,findings[0],d1,"Cond2")
+                write_row(ws,rx+1,id,findings[1],d2,"Cond2")
             #one finding with d1 and optional d2 - code named Cond1
             elif len(findings) == 1:
                 #print u"Row:%d:cond1:" % (rx) + findings[0] + "\t" + d1 + d2;
-                write_row(ws,rx+1,findings[0],d1+d2,"Cond1")
+                write_row(ws,rx+1,id,findings[0],d1+d2,"Cond1")
             #Cond4 - unmatched findings and finding details after line split
+            else:
+                write_row(ws,rx+1,id,finding,(d1 + "\n" + d2),"Cond4: Neelansha Please look at it!")
+        # Neelansha madam's special case - Cond2.1
+        elif numbered_findings and not numbered_finding_details and len(numbered_findings) == 2 and d1 != '' and d2 != '':
+            write_row(ws,rx+1,id,numbered_findings[0],d1,"Cond2.1")
+            write_row(ws,rx+1,id,numbered_findings[1],d2,"Cond2.1")
         else:
             #Cond4 - no such match. Dump findings and finding details and notify
-            write_row(ws,rx+1,finding,(d1 + "\n" + d2),"Cond4: Neelansha Please look at it!")
+            write_row(ws,rx+1,id,finding,(d1 + "\n" + d2),"Cond4: Neelansha Please look at it!")
 
 
     print 'Written file: ' + ifile + ".xls"    

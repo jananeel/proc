7f8413fd00e53f47382956d3411425a485b21d0c
neelansha <neelansha@yahoo.com>
2014-02-08 16:58:04 -0800

    Fix some style

---
+++ audit_writer.py
@@ -0,0 +1,43 @@
+import xlwt
+class AuditReportWriter:
+    ROW = 0
+    ID = 1
+    FINDING = 2
+    FINDING_DETAIL = 3
+    COMMENT = 4
+    TEXT_STYLE = xlwt.XFStyle()
+    TEXT_STYLE.alignment.wrap = 1
+    TEXT_STYLE_ERROR = xlwt.XFStyle()
+    TEXT_STYLE_ERROR.alignment.wrap = 1
+    HEADER_STYLE = xlwt.XFStyle()
+    HEADER_STYLE.alignment.wrap = 1
+
+    def __init__(self,xlname,sname):
+        self.xlname = xlname
+        self.sheet_name = sname
+        self.workbook = xlwt.Workbook()
+        self.worksheet = self.workbook.add_sheet(self.sheet_name)
+        self.write_rw = 0
+        self.write_row("Row #","ID", "Finding","Finding Detail", "Comment")
+        self.write_rw +=1
+
+        #set col width
+        self.worksheet.col(self.ROW).width = 256 * 3 #3 column width
+        self.worksheet.col(self.ID).width = 256 * 8
+        self.worksheet.col(self.FINDING).width = 256 * 50
+        self.worksheet.col(self.FINDING_DETAIL).width = 256 * 60
+        self.worksheet.col(self.COMMENT).width = 256 * 10
+        
+    def save(self):
+        self.workbook.save(self.xlname)
+
+    def write_row(self,row_num,id,finding,detail,comment):
+        style = self.TEXT_STYLE
+        if comment.startswith('Cond4'):
+            style = self.TEXT_STYLE_ERROR
+        self.worksheet.write(self.write_rw,self.ROW,row_num,style)
+        self.worksheet.write(self.write_rw,self.ID,id,style)
+        self.worksheet.write(self.write_rw,self.FINDING,finding,style,)
+        self.worksheet.write(self.write_rw,self.FINDING_DETAIL,detail,style)
+        self.worksheet.write(self.write_rw,self.COMMENT,comment,style)
+        self.write_rw +=1
--- excel.py
+++ excel.py
@@ -1,10 +1,9 @@
 import sys
 import utils
 import xlrd
-from xlwt import *
 import glob
 import logging
-from xlrd import XLRDError
+from audit_writer import AuditReportWriter
 
 #def removeNumerics(line):
 #    if(line.strip()[0].isdigit()):
@@ -17,25 +16,8 @@ logging.basicConfig(level=logging.DEBUG)
 # It keeps track of new workbook, row number, column number etc..
 # This will be the only dependency with xlwt
 
-ROW = 0
-ID = 1
-FINDING = 2
-FINDING_DETAIL = 3
-COMMENT = 4
-
-write_rw = 0
-
-def write_row(ws,row_num, id,finding, detail, comment):
-    global write_rw
-
-    ws.write(write_rw,ROW,row_num)
-    ws.write(write_rw,ID,id)
-    ws.write(write_rw,FINDING,finding)
-    ws.write(write_rw,FINDING_DETAIL,detail)
-    ws.write(write_rw,COMMENT,comment)
-    write_rw +=1
-
-
+        
+        
 ifiles = glob.glob(sys.argv[1])
 
 for ifile in ifiles:
@@ -44,17 +26,11 @@ for ifile in ifiles:
     try:
         book = xlrd.open_workbook(ifile,encoding_override="cp1252")
         sheet = book.sheet_by_name("Findings & CAP")
-    except XLRDError as e:
+    except xlrd.XLRDError as e:
         logging.exception("Unable to open -Findings & CAP- in file: " + ifile )
         continue
 
-    #define writer
-    #TODO: encapsulate the creation and initialization in to a method in a class
-    # leads to better abstraction of reads and writes. Easier to debug!!
-    write_rw = 0  
-    wb = Workbook()
-    ws = wb.add_sheet('0')
-    write_row(ws,"Row #","ID", "Finding","Finding Detail", "Comment")
+    ar = AuditReportWriter(ifile+'.xls','Findings & CAP')
 
     for rx in range(2,sheet.nrows):
         finding = sheet.cell_value(rowx=rx,colx=4) #Column E, row = rx
@@ -77,7 +53,7 @@ for ifile in ifiles:
         except:
             logging.exception("Error reading, row: " + str(rx))
             #try to write row back
-            write_row(ws,rx+1,id,finding,unicode(findingdetail1)+"\n" + unicode(findingdetail2),"Cond4: Neelansha Madam. Please look" + str(sys.exc_info()))
+            ar.write_row(rx+1,id,finding,unicode(findingdetail1)+"\n" + unicode(findingdetail2),"Cond4: Neelansha Madam. Please look" + str(sys.exc_info()))
             logging.exception(unicode(findingdetail1)+"\n" + unicode(findingdetail2))
             continue
             
@@ -85,33 +61,31 @@ for ifile in ifiles:
         (fhdr,numbered_findings) = utils.extract_numbered_entries("\n".join(findings))
         (fdhdr,numbered_finding_details) = utils.extract_numbered_entries(d1+'\n'+d2)
         
-
         #Check for numeric matches - code matched as Cond3
         if( numbered_findings and numbered_finding_details and len(numbered_findings) == len(numbered_finding_details)):
             for i in range(len(numbered_findings)):
                 #print u"Row:%d:cond3 numeric:" % (rx) + numbered_findings[i] + "\t" + numbered_finding_details[i]
-                write_row(ws,rx+1,id,fhdr + numbered_findings[i] ,fdhdr + numbered_finding_details[i],"Cond3: Numeric match")
+                ar.write_row(rx+1,id,fhdr + numbered_findings[i] ,fdhdr + numbered_finding_details[i],"Cond3: Numeric match")
         #Cond2 - two findings with d1 and d2
         elif not numbered_findings:
             #equality check for 2 - code named Cond2
             if len(findings) == 2 and d1 != '' and d2 != '':
-                write_row(ws,rx+1,id,findings[0],d1,"Cond2")
-                write_row(ws,rx+1,id,findings[1],d2,"Cond2")
+                ar.write_row(rx+1,id,findings[0],d1,"Cond2")
+                ar.write_row(rx+1,id,findings[1],d2,"Cond2")
             #one finding with d1 and optional d2 - code named Cond1
             elif len(findings) == 1:
                 #print u"Row:%d:cond1:" % (rx) + findings[0] + "\t" + d1 + d2;
-                write_row(ws,rx+1,id,findings[0],d1+d2,"Cond1")
+                ar.write_row(rx+1,id,findings[0],d1+d2,"Cond1")
             #Cond4 - unmatched findings and finding details after line split
             else:
-                write_row(ws,rx+1,id,finding,(d1 + "\n" + d2),"Cond4: Neelansha Please look at it!")
+                ar.write_row(rx+1,id,finding,(d1 + "\n" + d2),"Cond4: Neelansha Please look at it!")
         # Neelansha madam's special case - Cond2.1
         elif numbered_findings and (not numbered_finding_details or len(numbered_findings) != len(numbered_finding_details)):
             for finding in numbered_findings:
-                write_row(ws,rx+1,id,finding,d1+'\n'+d2,"Cond2.1")
+                ar.write_row(rx+1,id,finding,d1+'\n'+d2,"Cond2.1")
         else:
             #Cond4 - no such match. Dump findings and finding details and notify
-            write_row(ws,rx+1,id,finding,(d1 + "\n" + d2),"Cond4: Neelansha Please look at it!")
-
+            ar.write_row(rx+1,id,finding,(d1 + "\n" + d2),"Cond4: Neelansha Please look at it!")
 
     logging.info('Written file: ' + ifile + ".xls")    
-    wb.save(ifile + ".xls")
+    ar.save()

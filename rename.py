61757736aaa66726374ec7446437d2a3557d202d
neelansha <neelansha@yahoo.com>
2014-02-19 10:47:55 -0800

    Fix the target names to a lower case file name

--- rename.py
+++ rename.py
@@ -10,19 +10,33 @@ import os
 import getopt
 import sys
 import hashlib
+import glob
 
+FILE_PREFIX = 5
+increment_integer = 0
+def generate_file_name(basename):
+    global increment_integer,FILE_PREFIX
+    increment_integer += 1
+    reasonable_basename = ''.join(e for e in basename if e.isalnum()).lower()
+    return reasonable_basename[0:FILE_PREFIX] + str(increment_integer)
 
-optlist,gargs = getopt.getopt(sys.argv[1:],'c:s:e:d:')
+optlist,gargs = getopt.getopt(sys.argv[1:],'c:s:e:d:g:')
 optmap = dict(optlist)
 
 map_file = optmap.get("-c")
 sym_link = optmap.get("-s")
 extension = optmap.get("-e","")
 directory = optmap.get("-d","")
+fileglob = optmap.get("-g","")
+
+print directory
+
+#files = list(gargs)
+#if gargs and fileglob:
+files  = glob.glob(fileglob)
 
 #print optmap
 
-files = gargs
 #print files
 #print map_file
 
@@ -31,13 +45,15 @@ if map_file:
     with file(map_file) as f: file_contents = f.read()
     rename_map = dict( line.split('::') for line in file_contents)
 
+print files
 for fi in files:
+    print fi
     if os.path.isfile(fi):
-        tgt_folder = os.path.dirname(fi) if not directory else "."
+        tgt_folder = directory if directory and os.path.dirname(directory) else "."
         if rename_map:
             tgt_file = os.path.dirname(fi)+"/"+(rename_map[fi])
         else:
-            tgt_file = tgt_folder +"/" + hashlib.md5(fi.encode('utf-8')).hexdigest() + "." + extension
+            tgt_file = tgt_folder + "/" + generate_file_name(os.path.basename(fi)) #hashlib.md5(fi.encode('utf-8')).hexdigest() + "." + extension
 
         print tgt_file
         os.symlink(fi,tgt_file) if sym_link else os.rename(fi,tgt_file)
